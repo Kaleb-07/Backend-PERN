a32dc47803f2b7a6d95d007f2b030de3908bc5d7
Kaleb Mulugeta Estifanos <kalebmulugeta07@gmail.com>
2026-03-25 15:21:36 -0500

    Add API in authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,7 +2,7 @@ import express from "express";
 import { register, login, logout } from "../controllers/authControllers.js"
 
 const router = express.Router();
-
+// API
 router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", logout);
