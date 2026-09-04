e736f0c73543fb9c144864291bd877976e8c87f8
Kaleb Mulugeta Estifanos <kalebmulugeta07@gmail.com>
2026-09-04 12:58:14 -0500

    Update generateToken.js

--- backend/src/utils/generateToken.js
+++ backend/src/utils/generateToken.js
@@ -8,7 +8,7 @@ export const generateToken = (userId) => {
     });
 };
 
-// Sends token in HTTP cookies (only for controllers)
+// Sends token in HTTP cookies or only for controllers
 export const sendToken = (res, token) => {
     res.cookie("jwt", token, {
         httpOnly: true,
